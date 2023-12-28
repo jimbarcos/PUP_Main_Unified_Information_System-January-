2a89a355a785dc03d0e4395000a57fcc6ee7a4c7
ilhye <144883820+ilhye@users.noreply.github.com>
2023-12-28 15:28:07 +0800

    Initial commit

---
+++ src/GUIs_MainClass/MainClass.java
@@ -0,0 +1,9 @@
+package GUIs_MainClass;
+
+public class MainClass {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        LogIn logFrame = new LogIn();
+        logFrame.setVisible(true);
+    }
+}
